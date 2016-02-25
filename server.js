7b3b62f0a832e375c9b21cba43f4a0aece82d96d
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2016-02-25 20:09:46 -0300

    Added override responses option

--- server.js
+++ server.js
@@ -14,11 +14,63 @@ app.use(express.static('files'));
 _properties.bind.forEach(function (bindObject) {
 
     if (bindObject && bindObject.uri && bindObject.path) {
+
         app.use(bindObject.uri, express.static(_properties.root_folder + bindObject.path));
     } else {
+
         console.log('Invalid value to bind object: ', bindObject);
     }
 });
+
+var overrideResponses = _properties.override_responses;
+if (overrideResponses && overrideResponses.length) {
+
+    overrideResponses.forEach(function (overrideResponse) {
+
+        if (overrideResponse && overrideResponse.uri && overrideResponse.field && overrideResponse.value) {
+
+            app.use(overrideResponse.uri, function (req, res) {
+
+                var url = apiHost + overrideResponse.uri;
+
+                req.pipe(request(url, function (err, response, body) {
+
+                    body = JSON.parse(body);
+
+                    var field = overrideResponse.field;
+                    var value = overrideResponse.value;
+
+                    if (field.indexOf('.') > -1) {
+
+                        var splittedField = field.split('.');
+                        var lastField = splittedField[splittedField.length - 1];
+
+                        splittedField.reduce(function (object, property) {
+                            if (object && property) {
+
+                                if (property == lastField) {
+                                    object[property] = value;
+                                }
+
+                                return object[property];
+                            }
+                        }, body);
+                    } else {
+
+                        body[field] = value;
+                    }
+
+                    res.json(body);
+
+                    return body;
+                }));
+            });
+        } else {
+
+            console.log('Invalid value to override object: ', overrideResponse);
+        }
+    });
+}
 
 app.use('/', function (req, response) {
     var _request = request(apiHost + req.url);
@@ -29,6 +81,6 @@ app.use('/', function (req, response) {
 
 app.listen(_properties.api.port);
 
-console.info('Server started listen: ', _properties.api.port);
-console.info('Api Host: ', apiHost);
-console.info('Root folder: ', _properties.root_folder);
+console.info('Server started listen:', _properties.api.port);
+console.info('Api Host:', apiHost);
+console.info('Root folder:', _properties.root_folder);
